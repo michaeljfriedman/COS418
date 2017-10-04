ce13a391cea30c58bbe0180dfc2fc8ced83846a1
Michael J Friedman <mfred240@gmail.com>
2017-10-03 21:12:57 -0400

    [A1.3] Added Bash testing scripts; passes tests (on Linux)
    - No changes were made to the assignment itself, but tests report
      a socket error on Windows and pass on Linux. I assume this is
      some incompatibility problem between Windows and Linux sockets.

---
+++ A1.3-mapreduce2/src/mapreduce/test-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# test-all.sh
+# Author: Michael Friedman
+#
+# Runs all tests for the distributed part of the assignment.
+
+echo "----- Testing basic (w/o failures) -----"
+go test -v -run TestBasic
+echo
+
+echo "----- Testing with failures -----"
+go test -v -run Failure
+echo
---
+++ A1.3-mapreduce2/src/mapreduce/test-no-failure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# test-all.sh
+# Author: Michael Friedman
+#
+# Runs all tests for the distributed part of the assignment.
+
+go test -v -run TestBasic
